b94696d20352358a12355deaa49a03e96ff9f12c
Loic Segundo <loic.segundo@epita.fr>
2021-01-10 00:17:41 +0000

    bitcoin address generated from private key with uncompressed public key

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/hdrtpzzzq/btc_addresses
+
+go 1.15
+
+require (
+	github.com/btcsuite/btcutil v1.0.2
+	github.com/decred/dcrd/dcrec/secp256k1 v1.0.3
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
+)
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+	"crypto/ecdsa"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/btcsuite/btcutil/base58"
+	"github.com/decred/dcrd/dcrec/secp256k1"
+	"golang.org/x/crypto/ripemd160"
+)
+
+func getInput() *secp256k1.PublicKey {
+
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println(err, "Can't read the input for some reasons")
+		panic(err)
+	}
+
+	input = strings.TrimSpace(input)
+	inputSlice, err := hex.DecodeString(input)
+
+	if err != nil {
+		fmt.Println(err, "Unable to decode string into byte's slice")
+		panic(err)
+	}
+
+	_, pubKey := secp256k1.PrivKeyFromBytes(inputSlice)
+
+	return pubKey
+}
+
+func printEcdsaKey(key *ecdsa.PublicKey) {
+
+	x := fmt.Sprintf("0x%x", key.X)
+	y := fmt.Sprintf("0x%x", key.Y)
+	fmt.Println("The x coordinates of the key are:\n", x)
+	fmt.Println("The y coordinates of the key are:\n", y)
+
+}
+
+func sumRipemd160(hashKey []byte) []byte {
+
+	hasher := ripemd160.New()
+	hasher.Write([]byte(hashKey))
+	hashBytes := hasher.Sum(nil)
+
+	return hashBytes
+
+}
+
+func main() {
+
+	pubKey := getInput()
+	ecdsaPubKey := pubKey.ToECDSA()
+
+	pub := append(ecdsaPubKey.X.Bytes(), ecdsaPubKey.Y.Bytes()...)
+	pub = append([]byte{4}, pub...)
+	fmt.Println()
+
+	hashKey1 := sha256.Sum256(pub)
+
+	hashKey2 := append([]byte{0}, sumRipemd160(hashKey1[:])...)
+
+	hashKey3 := sha256.Sum256(hashKey2[:])
+
+	hashKey4 := sha256.Sum256(hashKey3[:])
+
+	checksum := hashKey4[:4]
+
+	hash := append(hashKey2, checksum...)
+
+	address := base58.Encode(hash)
+
+	fmt.Println("This is your btc address:\n", address)
+
+}
